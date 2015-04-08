3251d480aee1b0db79bc6cf2f3b8dc4078585e08
jfrey <nomhad@gmail.com>
2015-04-08 21:17:46 +0200

    AWB using 32F stream.

--- convert_raw/main.cpp
+++ convert_raw/main.cpp
@@ -57,6 +57,32 @@ void SimplestCB(Mat& in, Mat& out, float percent) {
     merge(tmpsplit,out);
 }
 
+// version with 32F matrix
+void SimplestCB32F(Mat& in, Mat& out, float percent) {
+    assert(in.channels() == 3);
+    assert(percent > 0 && percent < 100);
+ 
+    float half_percent = percent / 200.0f;
+ 
+    vector<Mat> tmpsplit; split(in,tmpsplit);
+    for(int i=0;i<3;i++) {
+        //find the low and high precentile values (based on the input percentile)
+        Mat flat; tmpsplit[i].reshape(1,1).copyTo(flat);
+        cv::sort(flat,flat,CV_SORT_EVERY_ROW + CV_SORT_ASCENDING);
+        float lowval32 = flat.at<float>(((float)flat.cols) * half_percent);
+        float highval32 = flat.at<float>(((float)flat.cols) * (1.0 - half_percent));
+	std::cout << "float: " << lowval32 << " " << highval32 << std::endl;
+        
+        //saturate below the low percentile and above the high percentile
+        tmpsplit[i].setTo(lowval32,tmpsplit[i] < lowval32);
+        tmpsplit[i].setTo(highval32,tmpsplit[i] > highval32);
+        
+        //scale the channel
+        normalize(tmpsplit[i],tmpsplit[i],0,1,NORM_MINMAX);
+    }
+    merge(tmpsplit,out);
+}
+
 /**
  * Convert a packed array of n elements with vw useful bits into array of
  * 8bit elements, dropping LSB.
@@ -128,10 +154,12 @@ int main ()
     cv::Mat out = Mat(ImageSize, CV_8UC1);
     cv::Mat colorz = Mat(ImageSize, CV_8UC3);
     cv::Mat wb = Mat(ImageSize, CV_8UC3);
+    cv::Mat colorz32 = Mat(ImageSize, CV_32FC3);
+    cv::Mat wb32 = Mat(ImageSize, CV_32FC3);
 
     cv::Mat mat16 = Mat(ImageSize, CV_8UC2);  //cvCreateImage(ImageSize , IPL_DEPTH_16U, 3 ); // Color image
 
-   cv::Mat mat16R = Mat(ImageSize, CV_8UC2);  //cvCreateImage(ImageSize , IPL_DEPTH_16U, 3 ); // Color image
+    cv::Mat mat16R = Mat(ImageSize, CV_8UC2);  //cvCreateImage(ImageSize , IPL_DEPTH_16U, 3 ); // Color image
 
     open_device((char*)"/dev/video0");
     init_device(ImageSize.width, ImageSize.height);
@@ -141,14 +169,11 @@ int main ()
 
     start_capturing();
 
-
-
-
     int rows = pOpenCVImage.rows;
-int cols = pOpenCVImage.cols;
-int num_el = rows*cols;
- int inputChan = 2;
-int len = num_el * inputChan;
+    int cols = pOpenCVImage.cols;
+    int num_el = rows*cols;
+    int inputChan = 2;
+    int len = num_el * inputChan;
 
 // printf("Start capturing - %d by %d by %d for %d -\n",  pOpenCVImage.rows, pOpenCVImage.cols, pOpenCVImage.elemSize1(),     len);
 
@@ -198,14 +223,14 @@ int len = num_el * inputChan;
 	  // Mat RGg2 =
 	  Mat g4 = Mat( channels[3], Rect(160, 0, 80, 120));
 
-	  	  imshow( "roi1",  g1 ); 
-	  	  imshow( "roi2",  b1); 
-	  	  imshow( "roi3",  g3); 
-	  	  imshow( "roi4",  b3); 
-	  	  imshow( "roi5",  r2); 
-	  	  imshow( "roi6",  g2); 
-	  	  imshow( "roi7",  r4); 
-	  	  imshow( "roi8",  g4); 
+	  // imshow( "roi1",  g1 ); 
+	  // imshow( "roi2",  b1); 
+	  // imshow( "roi3",  g3); 
+	  // imshow( "roi4",  b3); 
+	  // imshow( "roi5",  r2); 
+	  // imshow( "roi6",  g2); 
+	  // imshow( "roi7",  r4); 
+	  // imshow( "roi8",  g4); 
 	  
 	  // create back from 8 rois the bayer pattern
 	  // G1B1 G3B3 G1R1 ...
@@ -246,6 +271,17 @@ int len = num_el * inputChan;
 
 	  SimplestCB(colorz,wb,1);
 	  imshow("AWB",wb);
+
+
+	  //	  Mat toto = cv::Mat(ImageSize, CV_32FC1, ImageSize) ; 
+	  cv::normalize(colorz, colorz32, 0, 1, cv::NORM_MINMAX, CV_32FC3);
+
+	  //	  colorz.convertTo(colorz32, CV_32FC1); 
+	  //	  cvCvtScale(colorz,colorz32,1/255.,0);
+	  imshow("tadam32", colorz32);
+	  SimplestCB32F(colorz32,wb32,1);
+	  imshow("AWB32",wb32);
+
 	  //////////////////////////
 	  // print_mat(pOpenCVImage);
 
